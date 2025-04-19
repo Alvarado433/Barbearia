753c02f8f1bd0d50fa1d751345a0a7c837f3ee57
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2025-04-19 16:37:38 -0300

    editando o menu

--- src/components/site/Menu/Menu.tsx
+++ src/components/site/Menu/Menu.tsx
@@ -6,6 +6,13 @@ import React, { useState } from "react";
 
 const Menu = () => {
   const [menuAberto, setMenuAberto] = useState(false);
+  const [mostrarMensagem, setMostrarMensagem] = useState(false);
+
+  // Função para o agendamento (com notificação personalizada)
+  const agendamento = () => {
+    setMostrarMensagem(true); // Exibe a mensagem personalizada
+    setTimeout(() => setMostrarMensagem(false), 3000); // Fecha a mensagem após 3 segundos
+  };
 
   return (
     <header className="topo fixed-top">
@@ -21,18 +28,25 @@ const Menu = () => {
 
       <nav className="menu-topo d-none d-md-block">
         <ul className="d-flex justify-content-center">
-          <li><a href="#">Início</a></li>
-          <li><a href="#sobrenos">Sobre Nós</a></li>
-          <li><a href="#servicos">Serviços</a></li>
-          <li><a href="#promo">Promoções</a></li>
+          <li><Link href="#">Início</Link></li>
+          <li><Link href="#sobrenos">Sobre Nós</Link></li>
+          <li><Link href="#servicos">Serviços</Link></li>
+          <li><Link href="#promo">Promoções</Link></li>
         </ul>
       </nav>
 
       <div className="button">
-        <Link href="/agendar" className="btn btn-primary">
+        <Link href="#" onClick={agendamento} className="btn btn-primary">
           Agendamento
         </Link>
       </div>
+
+      {/* Exibindo a mensagem personalizada */}
+      {mostrarMensagem && (
+        <div className="alert alert-info fixed-top" role="alert">
+          Agendamento em breve! Fique ligado.
+        </div>
+      )}
 
       {/* Menu hamburguer (mobile) */}
       <div className="menu-hamburguer d-block d-md-none">
@@ -53,16 +67,16 @@ const Menu = () => {
           <nav className="navbar navbar-expand-lg">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <a className="nav-link" href="#">Início</a>
+                <Link className="nav-link" href="#">Início</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#sobrenos">Sobre Nós</a>
+                <Link className="nav-link" href="#sobrenos">Sobre Nós</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#servicos">Serviços</a>
+                <Link className="nav-link" href="#servicos">Serviços</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#promo">Promoções</a>
+                <Link className="nav-link" href="#promo">Promoções</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" href="/agendar">Agendamento</Link>
