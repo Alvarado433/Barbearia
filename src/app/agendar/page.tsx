c33efd2fd02258161290883361379a57a3783e23
Rhaian Alvarado <alvaradorhaian766@gmail.com>
2025-04-27 00:39:30 -0300

    adicionadno a pagina de agendar

---
+++ src/app/agendar/page.tsx
@@ -0,0 +1,9 @@
+export default function AgendarPage() {
+    return (
+      <div className="agendamento">
+        <h1>Agendar um Horário</h1>
+        <p>Em breve você poderá agendar seu corte por aqui!</p>
+      </div>
+    );
+  }
+  
